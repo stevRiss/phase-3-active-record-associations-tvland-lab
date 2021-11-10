ccd7fd41f6c1c93913aae0552aa0094d081bb4e2
Stephen <stevriss22@gmail.com>
2021-11-10 00:11:04 -0500

    commit tree 0168f107d7aaf3f7b248a5afc47656f99800ec1f

--- db/migrate/005_create_characters.rb
+++ db/migrate/005_create_characters.rb
@@ -1,5 +1,12 @@
 class CreateCharacters < ActiveRecord::Migration[6.1]
-  def change
-    # Your code here
+  def change 
+    create_table :characters do |t|
+      t.string :name
+      t.integer :actor_id
+      t.integer :show_id
+      t.string :catchphrase
+
+    end
+
   end
 end
